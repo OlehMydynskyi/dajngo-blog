29c2447cca10e47854511ee91291dba5f178bc9b
OlehMydynskyi <mydynskyioleh@gmai.com>
2024-03-21 11:41:55 +0200

    added registration

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -27,12 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'blogs:index'
+LOGOUT_REDIRECT_URL = 'blogs:index'
+
 
 # Application definition
 
 INSTALLED_APPS = [
     #My apps
     'blogs',
+    'acoounts',
 
     #Default apps
     'django.contrib.admin',
